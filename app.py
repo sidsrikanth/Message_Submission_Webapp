db04f0e84e34525eeb028df78397e9ff3c5afa45
Siddarth Srikanth <srikanthsiddarth1@gmail.com>
2023-02-16 02:03:09 -0800

    comments added

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import sqlite3
 app = Flask(__name__)
 
 DB_NAME = 'data/messages.db'
+NUM_MESSAGES = 5
 
 @app.route("/")
 def main():
-    return render_template('main.html') # replace with main.html
-    # create a base.html and make everything extend base.html
+    return render_template('main.html')
 
 @app.route("/submit/", methods = ['POST', 'GET'])
 def submit():
@@ -22,32 +22,19 @@ def submit():
 
 @app.route("/view/")
 def view():
-    messages = random_messages(5)
+    messages = random_messages(NUM_MESSAGES)
     return render_template("view.html", messages = messages)
 
 def get_message_db():
-    # write some helpful comments here
-    # try:
-    #     return g.message_db
-    # except:
-    #     g.message_db = sqlite3.connect("messages.db")
-    #     cmd = """CREATE TABLE IF NOT EXISTS messages (
-    #     id INTEGER NOT NULL IDENTITY PRIMARY KEY,
-    #     handle TEXT,
-    #     message TEXT,
-    #     );
-    #     """
-    #     cursor = g.message_db.cursor()
-    #     cursor.execute(cmd)
-    #     g.message_db.commit()
-    #     return g.message_db
-
+    # returns connection to database messages.db which stores users' messages
+    # check if database exists in g attribute
     try:
         return g.message_db
+    # if not, create database
     except:
         with sqlite3.connect(DB_NAME) as conn:
             cur = conn.cursor()
-
+            # set id column to store unique auto-incrementing values
             cmd = """CREATE TABLE IF NOT EXISTS messages (
             id INTEGER IDENTITY PRIMARY KEY,
             name TEXT,
@@ -58,25 +45,34 @@ def get_message_db():
             return g.message_db
 
 def insert_message(request):
-    # comments
+    # get connection to messages.db
     conn = get_message_db()
     cur = conn.cursor()
+
+    # get the form data
     name = request.form['name']
     message = request.form['message']
+
+    # insert form data into database
     cur.execute("INSERT INTO messages (name, message) VALUES (?, ?)", (name, message))
-    conn.commit()
+    conn.commit() # ensure row insertion has been saved
     conn.close()
 
 def random_messages(n):
-    # comments
+    # get connection to messages.db
     conn = get_message_db()
     cur = conn.cursor()
+
+    # select a random set of a specified number of messages
     cmd = '''SELECT * FROM messages
     ORDER BY RANDOM()
     LIMIT (?)
     '''
     cur.execute(cmd, (n,))
     rows = cur.fetchall()
+
+    # store all names and messages as a list of tuples
+    # row[1] corresponds to the name, row[2] to the message
     messages = [(row[1], row[2]) for row in rows]
     conn.close()
     return messages
